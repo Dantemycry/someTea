6e00b7b3bcd8ee96388e5f6e8c94f5ae47633cd6
Dante <123906224+Dantemycry@users.noreply.github.com>
2024-03-24 22:27:40 +0900

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-e51b82af-872f-461f-9af0-ac1a037bb4a1/"
 
 OUTPUT_DIR="logs"
 
